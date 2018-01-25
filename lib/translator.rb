3ccb731e5e2c5dd703e209eb611321779c8fdfcb
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-25 15:13:14 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,22 @@
 # require modules here
-
+require 'yaml'
 
 def load_library
-  require 'emoticons.yml'
+  YAML.load_file('emoticons.yml')
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(english)
+  load_library.each do |feeling, emos|
+    if emos.include?(english)
+      return emos[1]
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(japanese)
+  load_library.each do |feelingm emos|
+    if emos.include?(japanese)
+      return emos[0]
+    end
+  end
 end
